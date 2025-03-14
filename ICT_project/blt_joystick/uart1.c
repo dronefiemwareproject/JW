8f40157c5dd53a6de86cd1ea1cca379004d9495f
Jinwoo Woo <114578011+Jinunu99@users.noreply.github.com>
2025-03-15 00:31:29 +0900

    Add files via upload

---
+++ ICT_project/blt_joystick/uart1.c
@@ -0,0 +1,20 @@
+﻿#include "uart1.h"
+
+void UART1_Init()
+{
+	UCSR1A |= 1 << U2X1;
+	UBRR1H = 0;
+	UBRR1L = BAUD_9600;
+	UCSR1B |= 1 << RXEN1 | 1 << RXCIE1;
+	UCSR1B |= 1 << TXEN1; // | 1 << TXCIE0;
+}
+void UART1_Transmit(unsigned char data)
+{
+	while(!(UCSR1A & (1 << UDRE1)));
+	UDR1 = data;
+}
+unsigned char UART1_Receive()
+{
+	while(!(UCSR1A & (1 << RXC1)));
+	return UDR1;
+}
---
+++ ICT_project/blt_joystick/uart1.h
@@ -0,0 +1,9 @@
+﻿#ifndef UART1_H_
+#define UART1_H_
+
+#include "main.h"
+void UART1_Init();
+void UART1_Transmit(unsigned char data);
+unsigned char UART1_Receive();
+
+#endif /* UART1_H_ */
