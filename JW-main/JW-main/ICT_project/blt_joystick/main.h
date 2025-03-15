3cb20757e874c90a4651edc0fcd8e0b97418ee19
Jinwoo Woo <114578011+Jinunu99@users.noreply.github.com>
2025-03-15 16:45:27 +0900

    Add files via upload

---
+++ JW-main/JW-main/ICT_project/blt_joystick/main.h
@@ -0,0 +1,17 @@
+﻿
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+#include <string.h>
+#include <util/delay.h>
+#include "uart0.h"
+#include "uart1.h"
+#define BAUD_9600 207
+#define BAUD_38400 51
+#define BAUD_115200 16
+
+#endif /* MAIN_H_ */
---
+++ JW-main/JW-main/ICT_project/blt_joystick/uart0.h
@@ -0,0 +1,9 @@
+﻿#ifndef UART0_H_
+#define UART0_H_
+
+#include "main.h"
+void UART0_Init();
+//void UART0_Transmit(unsigned char data);
+//unsigned char UART0_Receive();
+
+#endif /* UART0_H_ */
